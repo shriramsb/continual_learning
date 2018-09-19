35742965b29fac59506ef432e06fbe1a10efb8a4
Shriram <sbshriram16@gmail.com>
2018-09-20 00:39:00 +0530

    Support for TPU

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import tensorflow as tf
 
 from tuner import HyperparameterTuner
 
+from tensorflow.contrib import tpu
+from tensorflow.contrib.cluster_resolver import TPUClusterResolver
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -11,16 +14,30 @@ def parse_args():
     parser.add_argument('--num_perms', type=int, default=5, help='the number of tasks')
     parser.add_argument('--trials', type=int, default=50, help='the number of hyperparameter trials per task')
     parser.add_argument('--epochs', type=int, default=100, help='the number of training epochs per task')
+    parser.add_argument('--use_tpu', type=bool, default=False, help='whether to use tpu')
+    parser.add_argument('--tpu_name', type=str, default="", help='name of tpu in the project')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    with tf.Session() as sess:
-        tuner = HyperparameterTuner(sess=sess, hidden_layers=args.hidden_layers, hidden_units=args.hidden_units,
-                                    num_perms=args.num_perms, trials=args.trials, epochs=args.epochs)
-        tuner.search()
-        print(tuner.best_parameters)
+    
+    if (not args.use_tpu):
+        with tf.Session() as sess:
+            tuner = HyperparameterTuner(sess=sess, hidden_layers=args.hidden_layers, hidden_units=args.hidden_units,
+                                        num_perms=args.num_perms, trials=args.trials, epochs=args.epochs)
+            tuner.search()
+            print(tuner.best_parameters)
+    else:
+        # Get the TPU's location
+        tpu_cluster = TPUClusterResolver(tpu=[args.tpu_name]).get_master()
+        with tf.Session(tpu_cluster) as sess:
+            sess.run(tpu.initialize_system())
+            tuner = HyperparameterTuner(sess=sess, hidden_layers=args.hidden_layers, hidden_units=args.hidden_units,
+                                        num_perms=args.num_perms, trials=args.trials, epochs=args.epochs)
+            tuner.search()
+            print(tuner.best_parameters)
+            sess.run(tpu.shutdown_system())
 
 if __name__ == "__main__":
     main()
